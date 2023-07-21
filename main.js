47c94a84d4e0372b25008c9d31b5c0a43ae43bb8
Sparke2021 <93227213+Sparke2021@users.noreply.github.com>
2023-07-21 17:45:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ function setup()
 {
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses);
 }
+
 
 function draw()
 {
