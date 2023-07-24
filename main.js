6be508afc2035adf0ccbf775c49d2af48a4266fc
Sparke2021 <93227213+Sparke2021@users.noreply.github.com>
2023-07-24 17:39:44 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+noseX=0;
+noseY=0;
+
 function preload()
 {
-
+  moustache = loadImage('https://i.postimg.cc/QdtMNxxz/moustacheimg.png');
 }
 
 function setup()
@@ -12,13 +15,32 @@ function setup()
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose' , gotPoses);
+    poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+        console.log("nose x = "+ results[0].pose.nose.x);
+        console.log("nose y = "+ results[0].pose.nose.y);
+    }
+}
 
 function draw()
 {
-
+  image (video, 0, 0, 300, 300);
+   image(moustache, noseX-30, noseY-16, 75, 75);
 }
 
 function take_snapshot()
